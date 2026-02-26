bf519d2e4076366f76bad8b4c05afc48b51dc5c3
aruzhan-jpg <seitzhanova.aruzhan@gmail.com>
2026-02-26 11:49:22 +0500

    Update all files

--- practice4/json_example.py
+++ practice4/json_example.py
@@ -1,32 +1,6 @@
-
-# Working with JSON
-
 import json
-
-person = {
-    "name": "Ali",
-    "age": 18,
-    "city": "Almaty"
-}
-
-json_string = json.dumps(person, indent=4)
-print("JSON string:")
-print(json_string)
-
-parsed_data = json.loads(json_string)
-print("Parsed name:", parsed_data["name"])
-
-with open("data.json", "w") as file:
-    json.dump(person, file, indent=4)
-
-with open("data.json", "r") as file:
-    data = json.load(file)
-    print("Read from file:", data)
-
-
-#2
 # some JSON:
-x =  '{ "name":"John", "age":30, "city":"New York"}'
+x =  '{ "name":"Ali", "age":18, "city":"New York"}'
 
 # parse x:
 y = json.loads(x)
@@ -38,8 +12,8 @@ print(y["age"])
 
 # a Python object (dict):
 x = {
-  "name": "John",
-  "age": 30,
+  "name": "Ali",
+  "age": 18,
   "city": "New York"
 }
 
@@ -48,3 +22,29 @@ y = json.dumps(x)
 
 # the result is a JSON string:
 print(y)
+
+print(json.dumps({"name": "Ali", "age": 18}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+
+x = {
+  "name": "John",
+  "age": 18,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
